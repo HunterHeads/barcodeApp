5f6e478935f9ae9174c5ed00f689317fb7977bc6
HunterHeads <kamiltomsia@gmail.com>
2018-07-03 17:02:10 +0200

    initial commit

---
+++ src/main/java/barcodeApp/BarcodeAppApplication.java
@@ -0,0 +1,12 @@
+package barcodeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarcodeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarcodeAppApplication.class, args);
+	}
+}
